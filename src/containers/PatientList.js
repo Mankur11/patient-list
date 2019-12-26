d3d45eef4330c8053923860c15f8822600020541
Mankur11 <mankur@inbox.ru>
2019-12-26 20:12:46 +0300

    implementation of displaying patient number by type, hovering

--- src/containers/PatientList.js
+++ src/containers/PatientList.js
@@ -4,20 +4,22 @@ import classes from '../containers/PatientList.module.css';
 class PatientList extends Component {
 
     state = {
-        data: 'empty',
+        presentData: '',
+        quittingData: '',
         historyNumber: '',
         firstName: '',
         lastName: '',
-        patrName: '',
         birthDate: '',
         diagnosis: '',
         bedNumber: '',
-        patientSelected: false
+        cause: '',
+        patientSelected: false,
+        tabSelectedRoom: 'Палата'
     };
 
     componentDidMount() {
-        async function parser() {
-            return await fetch("/data/presentList.json")
+        async function parser(url) {
+            return await fetch(url)
                 .then(response => response.json())
                 .then(responseData => {
                     return responseData;
@@ -25,82 +27,136 @@ class PatientList extends Component {
                 .catch(error => console.warn(error));
         }
 
-        parser().then(responseData => this.setState({data: responseData}));
+        parser("/data/presentList.json").then(responseData => this.setState({presentData: responseData}));
+        parser("/data/quittingList.json").then(responseData => this.setState({quittingData: responseData}));
+
+    }
+
+    numberOfPresentAndRetired(data) {
+        let number = 0;
+        for (let key in data) {
+            number++;
+        }
+        return number;
     }
 
     isSelectedPatient(item) {
-       this.setState({
-           diagnosis:item.diagnosis,
-           historyNumber: item.historyNumber,
-           firstName: item.firstName,
-           lastName:item.lastName,
-           patrName: item.patrName,
-           birthDate: item.birthDate,
-           bedNumber: item.bedNumber
-       });
+        this.setState({
+            diagnosis: item.diagnosis,
+            historyNumber: item.historyNumber,
+            firstName: item.firstName,
+            lastName: item.lastName,
+            birthDate: item.birthDate,
+            bedNumber: item.bedNumber,
+            patientSelected: true,
+            cause: item.cause
+        });
+    }
+
+    isClickedArrowButton() {
+        let selected = !this.state.patientSelected;
+        this.setState({
+            patientSelected: selected
+        });
+    }
+
+    isSelectedPresentPatient() {
+        this.setState({
+            tabSelectedRoom: 'Палата'
+        })
+    }
+
+    isSelectedRetiredPatient() {
+        this.setState({
+            tabSelectedRoom: 'Причина выбытия'
+        })
+    }
+
+    patientData(presentDataArr, retiredDataArr) {
+        return (
+            <table>
+                <tr className={classes.patientListHeader}>
+                    <a onClick={() => this.isSelectedPresentPatient()}>
+                        ПРИСУТСТВУЮТ({this.numberOfPresentAndRetired(this.state.presentData)})
+                    </a>
+                    <a onClick={() => this.isSelectedRetiredPatient()}>
+                        ВЫБЫВШИЕ({this.numberOfPresentAndRetired(this.state.quittingData)})
+                    </a>
+                </tr>
+                <tr className={classes.greyFont}>
+                    <td>№ИБ</td>
+                    <td>ФИО</td>
+                    <td>{this.state.tabSelectedRoom}</td>
+                </tr>
+                {this.state.tabSelectedRoom === 'Палата' ?
+                    presentDataArr.map(item => (
+                        <tr className={classes.hoveredPatient}>
+                            <td onClick={() => this.isSelectedPatient(item)}>{item.historyNumber} </td>
+                            <td onClick={() => this.isSelectedPatient(item)}>{item.firstName} {item.lastName}</td>
+                            <td onClick={() => this.isSelectedPatient(item)}>{item.bedNumber}</td>
+                        </tr>
+                    ))
+                    :
+                    retiredDataArr.map(item => (
+                        <tr className={classes.hoveredPatient}>
+                            <td onClick={() => this.isSelectedPatient(item)}>{item.historyNumber}</td>
+                            <td onClick={() => this.isSelectedPatient(item)}>{item.firstName} {item.lastName}</td>
+                            <td onClick={() => this.isSelectedPatient(item)}>{item.cause}</td>
+                        </tr>
+                    ))
+                }
+            </table>
+        )
     }
 
     patientInfo = () => {
         return (
             <div>
                 <table>
-                    <tr className={classes.patientInfoHeader}><a>Информация о пациенте</a></tr>
+                    <tr className={classes.patientInfoHeader}>Информация о пациенте
+                        <a href={''} onClick={() => this.isClickedArrowButton()}>&#60;</a>
+                    </tr>
                     <td>
-                        <tr>ФИО</tr>
-                        <tr>Возраст</tr>
-                        <tr>Диагноз</tr>
+                        <tr>ФИО {this.state.firstName} {this.state.lastName}</tr>
+                        <tr>Возраст {this.state.birthDate}</tr>
+                        <tr>Диагноз {this.state.diagnosis}</tr>
                     </td>
-                    <tr className={classes.patientInfoHeader}></tr>
-                    <td>
-                        <tr>{this.state.firstName} {this.state.lastName}</tr>
-                        <tr>{this.state.birthDate}</tr>
-                        <tr>{this.state.diagnosis}</tr>
-                    </td>
+                </table>
+            </div>
+        )
+    };
+
+    patientInfoEmptyPlace = () => {
+        return (
+            <div>
+                <table>
+                    <tr onClick={() => this.isClickedArrowButton()}>&#60;</tr>
                 </table>
             </div>
         )
     };
 
     patientLists = () => {
-        const dataArr = [];
-        for (let key in this.state.data) {
-            dataArr.push(this.state.data[key]);
+        const presentDataArr = [];
+        const retiredDataArr = [];
+        for (let key in this.state.presentData) {
+            presentDataArr.push(this.state.presentData[key]);
+        }
+        for (let key in this.state.quittingData) {
+            retiredDataArr.push(this.state.quittingData[key]);
         }
 
         return (
             <div>
-                <table>
-                    <tr className={classes.patientListHeader}><a>Присутствуют</a></tr>
-                    <tr>
-                        <td>№ИБ</td>
-                        <td>ФИО</td>
-                        <td>Палата</td>
-                    </tr>
-                    <td>
-                        {dataArr.map(item => (
-                            <tr>{item.historyNumber}</tr>
-                        ))}
-                    </td>
-                    <td>
-                        {dataArr.map(item => (
-                            <tr onClick={() => this.isSelectedPatient(item)}>{item.firstName} {item.lastName}</tr>
-                        ))}
-                    </td>
-                    <td>
-                        {dataArr.map(item => (
-                            <tr>{item.bedNumber}</tr>
-                        ))}
-                    </td>
-                </table>
+                {this.patientData(presentDataArr, retiredDataArr)}
             </div>
         )
     };
 
     render() {
-
         return (
             <div className={classes.PatientListWrapper}>
-                {this.patientInfo()}
+                {this.state.patientSelected ? this.patientInfo() : this.patientInfoEmptyPlace()}
                 {this.patientLists()}
             </div>
         )
